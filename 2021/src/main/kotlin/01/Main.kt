62a5c9df2ed150c7ee2d798dd00b7970b1161567
Florian <fcangrand@arca-computing.fr>
2021-12-01 13:38:22 +0100

    exo1

--- 2021/src/main/kotlin/01/Main.kt
+++ 2021/src/main/kotlin/01/Main.kt
@@ -8,8 +8,42 @@ fun main(args: Array<String>) {
 
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Lines: $lines")
-    for (s in lines) {
-        println("Line: $s")
+    println(exo1(lines.map { it.toInt() }))
+    println(exo1WithMap(lines.map { it.toInt() }))
+    println(exo12WithMap(lines.map { it.toInt() }))
+
+}
+
+private fun exo1(lines: List<Int>): Int{
+    var nbIncreased = 0
+    var previousVal = lines[0]
+    for (value in lines) {
+       if (value > previousVal) {
+           nbIncreased++
+       }
+        previousVal = value
     }
+    return nbIncreased
+}
+
+private fun exo1WithMap(lines: List<Int>): Int {
+    return lines.mapIndexed { index, value ->
+        if (index != 0 && lines[index - 1] < value) 1 else 0
+    }.sum()
+}
+
+
+private fun exo12WithMap(lines: List<Int>): Int {
+    return lines.mapIndexed { index, value -> compareWithPrevious(lines, index, value)
+    }.sum()
+}
+
+fun compareWithPrevious(lines: List<Int>, index: Int, value: Int) : Int{
+    if (index < 3) {
+        return 0
+    }
+    val commonSum = lines[index-2] + lines[index-1]
+    val previousSum = lines[index-3] + commonSum
+    val currentSum =  value + commonSum
+    return if (previousSum < currentSum) 1 else 0
 }
